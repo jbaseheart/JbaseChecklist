a8d11f3e84a661790fd07e1026a251d7bed2edb7
jbase <jonathan.baseheart@gmail.com>
2017-08-23 23:22:00 -0500

    Updated the About page.

--- JbaseChecklist.App/Controllers/HomeController.cs
+++ JbaseChecklist.App/Controllers/HomeController.cs
@@ -17,8 +17,6 @@ namespace JbaseChecklist.App.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
